f0213cdc5710746ffa6d4b9dd759c75b31b93380
Ignatius Chandra <ignasiosc@gmail.com>
2019-08-16 02:47:53 +0700

    update warna action bar, penyesuaian desain register, buat awal halaman informasi

--- app/src/main/java/blackskystudio/com/dinaskebersihan/Register.java
+++ app/src/main/java/blackskystudio/com/dinaskebersihan/Register.java
@@ -9,5 +9,9 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
